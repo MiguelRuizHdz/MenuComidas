1bcc2d4bb6181230bc54898f409e62b28d5a35b0
adanh <adanh@ADAN-ACER-11>
2020-12-06 17:35:14 -0600

    Se escribio Hola amigos

--- src/menucomidas/MenuComidas.java
+++ src/menucomidas/MenuComidas.java
@@ -1,22 +1,14 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Proyecto Base de Datos - Menú de Comdas
  */
 package menucomidas;
 
-/**
- *
- * @author Miguel Ruiz
- */
 public class MenuComidas {
-
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola Mundo");
+        System.out.println("Hola amigos mios");
     }
     
 }
