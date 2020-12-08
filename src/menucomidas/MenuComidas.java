8fb31bdfc9c6cebc5d279806b8274e83ff007f37
Miguel Ruiz <miguel.ruizhz@uanl.edu.mx>
2020-12-08 16:13:12 -0600

    Ventanas

--- src/menucomidas/MenuComidas.java
+++ src/menucomidas/MenuComidas.java
@@ -16,7 +16,10 @@ public class MenuComidas {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hola Mundo");
+        Inicio ver = new Inicio();
+        ver.setVisible(true);
+        ver.setLocationRelativeTo(null);
+                
     }
     
 }
